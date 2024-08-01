214705c3a923ff471daea94ae8811419bbcecb32
Oliver Hernandez <oliverhernandez206@gmail.com>
2024-08-01 13:55:34 -0600

    binding bidireccional

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { NgModel } from '@angular/forms';
+
 
 @Component({
   selector: 'app-empleado',
   templateUrl: './empleado.component.html',
+  //template: "<p>Aqui va un empleado</p>",
   styleUrl: './empleado.component.css'
+  //styles: ["p{background-color:red}"]
 })
-export class EmpleadoComponent {
+export class EmpleadoComponent implements OnInit {
+
+  @Input() genero: string = "";
+  nombre = "Oliver"
+  apellido = "Hernandez"
+  //private edad = 22
+  edad: any = 22
+  empresa = "Google"
+  /*   getEdad() {
+      return this.edad;
+    }
+   */
+
+  habilitacionInput = false
+  usuRegistrado = false
+  textoDeRegistro = "No hay nadie registrado";
+
+  getRegistroUsuario() {
+    this.usuRegistrado = true
+  }
+
+
+  setUsuarioRegistrado(event: any) {
+    //alert("El usuario se acaba de registrar")
+    event.target.value == 'si' ? this.textoDeRegistro = "El usuario se acaba de registrar" : this.textoDeRegistro = "No hay nadie registrado";
+  }
+
+  constructor() { }
+  ngOnInit(): void { }
 
 }
