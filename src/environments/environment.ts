6d0fd3781e22d14a864ce59fe173c74419f0b4d7
Oliver Hernandez <oliverhernandez206@gmail.com>
2024-08-12 11:30:38 -0600

    Agregar environment.ts al gitignore y eliminarlo del seguimiento

--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyBshe6tKti1u6vTLowzQd3VBgxSQ-QfCLQ",
-    authDomain: "mis-clientes-68b89.firebaseapp.com",
-    databaseURL: "https://mis-clientes-68b89-default-rtdb.firebaseio.com",
-    projectId: "mis-clientes-68b89",
-    storageBucket: "mis-clientes-68b89.appspot.com",
-    messagingSenderId: "713256337463",
-    appId: "1:713256337463:web:c9b960654117a341173fb2"
-  };
